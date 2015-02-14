9c231c7f9f96542d38daa1a1fa587c483b8e8c1c
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2015-02-14 13:28:39 -0500

    Baseline starting point

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-console.clear();
 var beerData = JSON.parse(document.getElementById("beerData").textContent);
 var beerTemplate = document.getElementById("tmpl-beer").textContent;
 var beerList = document.getElementById("beerList");
 var filters = document.getElementById("filters");
+var filterLinks = filters.querySelectorAll("a");
 
 beerList.innerHTML = _.template(beerTemplate)(beerData);
 
 filters.addEventListener('click', function (e) {
   e.preventDefault();
   var beers = beerData.beers;
-  var filter = e.target.dataset.filter;
+  var clicked = e.target;
+  var filter = clicked.dataset.filter;
   var filteredBeers = [];
   var i;
+
+  for (i=0; i<filterLinks.length; i++) {
+    filterLinks[i].classList.remove('btn-active');
+  }
+
+  clicked.classList.add('btn-active');
       
   switch (filter) {
+    case 'all':
+      filteredBeers = beers;
+      break;
     case 'domestic':
       for (i=0; i<beers.length; i++) {
         if (beers[i].locale === 'domestic') {
@@ -28,9 +38,23 @@ filters.addEventListener('click', function (e) {
         }
       }
       break;
-    case 'craft':
+    case 'ale':
       for (i=0; i<beers.length; i++) {
-        if (beers[i].craft) {
+        if (beers[i].type === 'ipa' || beers[i].type === 'ale') {
+          filteredBeers.push(beers[i]);
+        }
+      }
+      break;
+    case 'lager':
+      for (i=0; i<beers.length; i++) {
+        if (beers[i].type === 'lager') {
+          filteredBeers.push(beers[i]);
+        }
+      }
+      break;
+    case 'stout':
+      for (i=0; i<beers.length; i++) {
+        if (beers[i].type === 'stout') {
           filteredBeers.push(beers[i]);
         }
       }
