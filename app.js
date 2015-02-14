0e4f3f93364ac6c1659b1fff0f212862757c3538
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2015-02-14 13:52:36 -0500

    Made it more declarative

--- app.js
+++ app.js
@@ -1,65 +1,74 @@
 var beerData = JSON.parse(document.getElementById("beerData").textContent);
+var beers = beerData.beers;
 var beerTemplate = document.getElementById("tmpl-beer").textContent;
 var beerList = document.getElementById("beerList");
 var filters = document.getElementById("filters");
 var filterLinks = filters.querySelectorAll("a");
 
-beerList.innerHTML = _.template(beerTemplate)(beerData);
+function loadBeers(beers) {
+  beerList.innerHTML = _.template(beerTemplate)({ beers: beers });
+}
+
+function setActiveFilter(active) {
+  for (i=0; i<filterLinks.length; i++) {
+    filterLinks[i].classList.remove('btn-active');
+  }
+  active.classList.add('btn-active');
+}
+
+function filterBeers(property, values) {
+  var filtered = [];
+  for (i=0; i<beers.length; i++) {
+    if (compareValues(beers[i], property, values)) {
+      filtered.push(beers[i]);
+    }
+  }
+  return filtered;
+}
+
+function compareValues(item, property, values) {
+  if (!Array.isArray(values)) {
+    return item[property] === values;
+  }
+  for (var i=0; i<values.length; i++) {
+    if (item[property] === values[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+loadBeers(beers);
 
 filters.addEventListener('click', function (e) {
   e.preventDefault();
-  var beers = beerData.beers;
   var clicked = e.target;
   var filter = clicked.dataset.filter;
   var filteredBeers = [];
   var i;
 
-  for (i=0; i<filterLinks.length; i++) {
-    filterLinks[i].classList.remove('btn-active');
-  }
-
-  clicked.classList.add('btn-active');
+  setActiveFilter(clicked);
       
   switch (filter) {
     case 'all':
       filteredBeers = beers;
       break;
     case 'domestic':
-      for (i=0; i<beers.length; i++) {
-        if (beers[i].locale === 'domestic') {
-          filteredBeers.push(beers[i]);
-        }
-      }
+      filteredBeers = filterBeers('locale', 'domestic');
       break;
     case 'imports':
-      for (i=0; i<beers.length; i++) {
-        if (beers[i].locale === 'import') {
-          filteredBeers.push(beers[i]);
-        }
-      }
+      filteredBeers = filterBeers('locale', 'import');
       break;
     case 'ale':
-      for (i=0; i<beers.length; i++) {
-        if (beers[i].type === 'ipa' || beers[i].type === 'ale') {
-          filteredBeers.push(beers[i]);
-        }
-      }
+      filteredBeers = filterBeers('type', ['ipa', 'ale']);
       break;
     case 'lager':
-      for (i=0; i<beers.length; i++) {
-        if (beers[i].type === 'lager') {
-          filteredBeers.push(beers[i]);
-        }
-      }
+      filteredBeers = filterBeers('type', 'lager');
       break;
     case 'stout':
-      for (i=0; i<beers.length; i++) {
-        if (beers[i].type === 'stout') {
-          filteredBeers.push(beers[i]);
-        }
-      }
+      filteredBeers = filterBeers('type', 'stout');
       break;
   }
     
-  beerList.innerHTML = _.template(beerTemplate)({ beers: filteredBeers });
+  loadBeers(filteredBeers);
 });
